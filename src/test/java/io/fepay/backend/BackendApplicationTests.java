502ebc7eeae0298c5eccd6f155f9d5a3f414d8f0
Jaehoon Kim <by09115@outlook.kr>
2019-11-21 16:58:30 +0900

    ADD: initial commit

---
+++ src/test/java/io/fepay/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.fepay.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
